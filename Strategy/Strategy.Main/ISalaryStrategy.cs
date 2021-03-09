a380776b6942e4676591c30a9076b20eb86df581
Bartłomiej Michalski <bartlomiej.j.michalski@gmail.com>
2021-03-09 11:30:41 +0100

    Removed unused files.

--- Strategy/Strategy.Main/ISalaryStrategy.cs
+++
@@ -1,7 +0,0 @@
-namespace Strategy.Main
-{
-    public interface ISalaryStrategy 
-    {
-        double Calculate(double @base, double sumOfIncomes, double employeeIncome, double importantCalculationFactor);
-    }
-}
--- Strategy/Strategy.Main/Salary/WorkerStrategy.cs
+++ Strategy/Strategy.Main/Salary/WorkerStrategy.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace Strategy.Main.Salary
 {
     public class WorkerStrategy : ISalaryStrategy
@@ -13,42 +11,4 @@ namespace Strategy.Main.Salary
             return @base + employeeIncome * importantCalculationFactor;
         }
     }
-  public class Product
-  {
-    public string Name { get; set; }
-  }
-
-  public class Order
-  {
-    public List<Product> Products { get; set; }
-    public Invoice Invoice { get; set; }
-  }
-
-
-  public class Invoice
-  {
-    public string Address { get; set; }
-    public string Nip { get; set; }
-  }
-
-  public class OrderBuilder
-  {
-    private List<Product> _products;
-    // many other parameters
-    public void AddProduct(Product product)
-    {
-      _products.Add(product);
-    }
-    // and many other methods
-
-    public Order Build() => new Order 
-    {
-      Products = _products,
-      /* and many others */
-    };
-  }
-
-
-
-
 }
